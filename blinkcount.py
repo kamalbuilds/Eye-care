da0e31d66b8faa449831fa49bdbd471016635c5c
kamal nayan <genuisamansingh@gmail.com>
2022-01-08 12:54:26 +0530

    checking the file

---
+++ blinkcount.py
@@ -0,0 +1,11 @@
+import cv2
+import cvzone
+
+cap = cv2.VideoCapture('Video.mp4')
+
+while True:
+    success, img = cap.read()
+    img=cv2.resize(img, (640,360))
+    cv2.imshow("kamal",img)
+    cv2.waitKey(1)
+    
